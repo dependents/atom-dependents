a77f65a6ac9e229f0d4866a0ea6aaa8e6990bf44
Joel Kemp <mrjoelkemp@gmail.com>
2016-06-08 16:45:01 -0400

    Initial skeleton

---
+++ spec/atom-dependents-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import AtomDependentsView from '../lib/atom-dependents-view';
+
+describe('AtomDependentsView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
