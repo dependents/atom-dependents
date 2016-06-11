eed89f5497993a10f1766a75f0c55b30a138d199
Joel Kemp <mrjoelkemp@gmail.com>
2016-06-11 13:23:11 -0400

    Progress bar added
    * Defers the execution of the command to the next tick so that
    the progress bar could render before being blocked by the
    sync command execution.
    
    Fixes #10

--- spec/atom-dependents-view-spec.js
+++
@@ -1,9 +0,0 @@
-'use babel';
-
-import AtomDependentsView from '../lib/atom-dependents-view';
-
-describe('AtomDependentsView', () => {
-  it('has one valid test', () => {
-    expect('life').toBe('easy');
-  });
-});
